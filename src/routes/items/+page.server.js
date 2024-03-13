2c0dff330a4ffd6e4b816434608615291faba95a
Prd46 <prd46@drexel.edu>
2024-03-12 21:16:28 -0400

    FIXING THE LIBERALS

--- src/routes/items/+page.server.js
+++ src/routes/items/+page.server.js
@@ -1,5 +1,5 @@
-import { PUBLIC_SUPABASE_TABLE } from "$env/static/public"; 
-import { supabase } from '../supabase_client';
+import { PUBLIC_SUPABASE_TABLE } from "$env/static/public";
+import { supabase } from "$lib/server/supabase_client";
 
 /**
  * Loads products from the 'product_info' table in Supabase.
@@ -11,11 +11,6 @@ export async function load() {
   const { data } = await supabase.from(PUBLIC_SUPABASE_TABLE).select("*"); //THIS IS THE EQUIVALENT OF SELECT * FROM DATABASE
   // console.log(data); // LOG THE DATA TO CHECK IF IT'S COMING IN
   return {
-    items: data ?? [] //OUTPUT YOUR DATA, WHICH WILL BE AN ARRAY. IF THERE'S NOT, SEND AN EMPTY ARRAY. 
+    items: data ?? [], //OUTPUT YOUR DATA, WHICH WILL BE AN ARRAY. IF THERE'S NOT, SEND AN EMPTY ARRAY.
   };
 }
-
-
-
-
-
--- src/routes/items/[slug]/+page.server.js
+++ src/routes/items/[slug]/+page.server.js
@@ -1,7 +1,7 @@
 // src/routes/[slug]/+page.server.js
 import { error } from "@sveltejs/kit";
 import { PUBLIC_SUPABASE_TABLE } from "$env/static/public";
-import { supabase } from '../../supabase_client';
+import { supabase } from "$lib/server/supabase_client";
 
 /**
  * Loads products from the 'product_info' table in Supabase.
@@ -10,18 +10,18 @@ import { supabase } from '../../supabase_client';
  * @returns {Promise<{ items: item[] }>} The product info.
  */
 
-export async function load({params}) {
+export async function load({ params }) {
   const { slug } = params; // Get the slug from the URL
   const { data, error } = await supabase
-  .from(PUBLIC_SUPABASE_TABLE)
-  .select("*")
-  .eq('id', slug)
-  .single(); // Select more specific data
+    .from(PUBLIC_SUPABASE_TABLE)
+    .select("*")
+    .eq("id", slug)
+    .single(); // Select more specific data
   // console.log(data);
-  
+
   if (!data) throw error(404);
 
   return {
-      item: data ?? [],
+    item: data ?? [],
   };
 }
